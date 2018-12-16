c60fd9a979e5c72b27192c5ba80b3abe636a4864
Roman Semizarov <semizarov@yandex-team.ru>
2018-12-16 16:19:59 +0300

    Fix audio formatting

--- www/sites/all/modules/chgk_db/classes/DbField.class.php
+++ www/sites/all/modules/chgk_db/classes/DbField.class.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__)."/DbField/DbFieldQuestion.class.php");
 
 class DbField {
   private $field;
-  protected $value; 
+  protected $value;
   protected $number;
   protected $html;
   protected $paragraphs;
@@ -18,7 +18,7 @@ class DbField {
   const SOUNDS_BASE_URL='/sounds/db';
 #  const MP3_PLAYER_TEMPLATE= '<embed type="application/x-shockwave-flash" src="http://www.google.com/reader/ui/3523697345-audio-player.swf?audioUrl=%s"  allowscriptaccess="never" width="400" height="27" quality="best" bgcolor="#ff0000" wmode="window" flashvars="playerMode=embedded" />';
 //  const MP3_PLAYER_TEMPLATE= '<embed type="application/x-shockwave-flash" src="/sounds/3523697345-audio-player.swf?audioUrl=%s"  allowscriptaccess="never" width="400" height="27" quality="best" bgcolor="#ff0000" wmode="window" flashvars="playerMode=embedded" />';
-  const MP3_PLAYER_TEMPLATE = '<p><audio src="%s" controls></audio></p>';
+  const MP3_PLAYER_TEMPLATE = '<audio src="%s" controls></audio>';
 
   public function __construct($field, $value, $number = false, $parent = null) {
     global $base_url;
@@ -33,9 +33,9 @@ class DbField {
 
   public function getCssClass() {
     return $this->field;
-  }  
+  }
   public function getHtml() {
-    
+
     if ($this->html) {
       return $this->html;
     }
@@ -44,7 +44,7 @@ class DbField {
     $this->formatHtml();
     return $this->html;
   }
-  
+
   public function getText() {
     return $this->value;
   }
@@ -54,22 +54,22 @@ class DbField {
             '<strong>'.$this->getName().
             ':</strong> '. $this->paragraphs[0];
   }
-  
+
   protected function updateFirstParagraphFb2() {
     $this->paragraphs[0] =
             '<strong>'.$this->getFb2Name().
             ':</strong> '. $this->paragraphs[0];
   }
-  
-                                                              
+
+
   public function getXML() {
      return "<".$this->field."><![CDATA[".$this->getHtml()."]]></".$this->field.">";
   }
-  
+
   public function getFb2() {
     if ($this->fb2) {
       return $this->fb2;
-    }    
+    }
     $this->fb2 = $this->value;
     $this->fb2 = html_entity_decode($this->fb2, ENT_COMPAT, 'UTF-8');
     $this->fb2 = htmlspecialchars($this->fb2, ENT_NOQUOTES, 'UTF-8');
@@ -195,7 +195,7 @@ class DbField {
         "<div class=\"razdatka\"><div class=\"razdatka_header\">Раздаточный материал</div> \\1</div>\n",
          $this->html  );
 
-    $this->html = preg_replace('/^\s+/m', "<br />\n&nbsp;&nbsp;&nbsp;&nbsp;", $this->html);  
+    $this->html = preg_replace('/^\s+/m', "<br />\n&nbsp;&nbsp;&nbsp;&nbsp;", $this->html);
 
     if (!preg_match('/^\|/m',$this->html)) {
       $this->html = preg_replace('/\s+\&#0150/m','&nbsp;\&#0150', $this->html);
@@ -211,9 +211,9 @@ class DbField {
     $this->addSoundsToHtml();
     if ($this->getSearchString()) {
         $this->highLight();
-    }    
+    }
   }
-  
+
   protected function getImageUrl( $name  ) {
     if ( preg_match( '/^\d{8}/', $name ) ) {
       $result =  $this->getImageBaseUrl()."/".$name;
@@ -223,8 +223,8 @@ class DbField {
     if ( !$result ) $result = $name;
     return $result;
   }
-  
-  
+
+
   protected function addSoundsToHtml() {
     $url = $this->getSoundsBaseUrl().'/$1';
     $playerTemplate = self::MP3_PLAYER_TEMPLATE;
@@ -232,7 +232,7 @@ class DbField {
     $this->html = preg_replace('/\(aud: ([^\)]*)\)/',$playerString, $this->html);
 
   }
-  
+
   protected function highLight() {
       $sstr = $this->getSearchString();
       setlocale(LC_ALL, 'ru_RU.utf8');
@@ -255,7 +255,7 @@ class DbField {
   public function getName( $nohtml = false ) {
     return $this->field;
   }
-  
+
   public function getFb2Name() {
     return $this->field;
   }
@@ -266,8 +266,8 @@ class DbField {
 
   public function isEmpty() {
     return $this->value === NULL || $this->value==='';
-  } 
-  
+  }
+
   public function getValue() {
     return $this->value;
   }
@@ -285,7 +285,7 @@ class DbField {
     }
     return $this->parent->getSearchString();
   }
-  
+
   protected function getImageBaseUrl() {
     return $this->imageBaseUrl;
   }
@@ -301,7 +301,7 @@ class DbField {
   public function setForPrint($flag= TRUE) {
     $this->isForPrint = $flag;
   }
-  
+
   public function setValue( $value ) {
     $this->value = $value;
   }
